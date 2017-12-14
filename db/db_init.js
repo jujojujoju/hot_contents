baf6624a920836b2e65f1633548f365965beda74
joju <whwndud92@gmail.com>
2017-12-14 11:33:55 +0900

    modify db config

--- db/db_init.js
+++ db/db_init.js
@@ -6,27 +6,27 @@ var asyncjs = require('async');
 if (!jinst.isJvmCreated()) {
     jinst.addOption("-Xrs");
     //convert between version
-    jinst.setupClasspath(['./drivers/ojdbc8.jar']);
+    // jinst.setupClasspath(['./drivers/ojdbc8.jar']);
 
     //*******************tibero version
     //jinst.setupClasspath(['./drivers/tibero6-jdbc.jar']);
 
-    // jinst.setupClasspath(['./drivers/ojdbc7.jar']);
+    jinst.setupClasspath(['./drivers/ojdbc7.jar']);
 }
 
-//var db_config = require('./db_config.json');
-// var db_config = {
-//     // Required
-//     url: 'jdbc:tibero:thin:@localhost:8629:tibero',
-//
-//     // Optional
-//     drivername: 'com.tmax.tibero.jdbc.TbDriver',
-//     minpoolsize: 10,
-//     maxpoolsize: 100,
-//
-//     properties: {user: "HR", password: "tibero"}
-// };
-var db = new JDBC(require('./db_config.json'));
+// var db_config = require('./db_config.json');
+var db_config = {
+    // Required
+    url: 'jdbc:tibero:thin:@localhost:8629:tibero',
+
+    // Optional
+    drivername: 'com.tmax.tibero.jdbc.TbDriver',
+    minpoolsize: 10,
+    maxpoolsize: 100,
+
+    properties: {user: "HR", password: "tibero"}
+};
+var db = new JDBC(db_config);
 
 var db_init = {
     init: function (callback) {
